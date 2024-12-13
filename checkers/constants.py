7315bb839dc4491ea3ae2fc36b9dc2d47154b118
Alastair Drew-Penn <alastair.drewpenn@gmail.com>
2024-12-13 15:40:41 -0500

    Starting simple pygame checkers loop

---
+++ checkers/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+# dimensions
+HEIGHT, WIDTH = 640, 640
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
